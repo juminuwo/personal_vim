01db330afa48d77c3559b8c5e6a053e5fe9bac03
Ubuntu <datascience@amswbj08-ds.dcbopage5zkuzjnox32240ipia.ax.internal.cloudapp.net>
2019-01-25 13:02:13 +0000

    update

--- personal_vim/install.sh
+++ personal_vim/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 BASEDIR=$(dirname "$0")
 
 mv ~/.vimrc ~/.vimrc_old
-cp $BASEDIR/.vim ~/.vimrc
+cp $BASEDIR/.vimrc ~/.vimrc
+apt install neovim
+echo "set runtime^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc" > ~/testvim

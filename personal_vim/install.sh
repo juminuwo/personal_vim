98afa4651519bf7d50959c5f55e9ee9f3e16e4d1
juminuwo <31014579+juminuwo@users.noreply.github.com>
2019-04-09 11:53:59 +0100

    not neovim anymore

--- personal_vim/install.sh
+++ personal_vim/install.sh
@@ -5,10 +5,10 @@ if [ -f ~/.vimrc ]; then
 	mv ~/.vimrc ~/.vimrc_old
 fi
 cp $BASEDIR/.vimrc ~/.vimrc
-apt install neovim
+#apt install neovim
 mkdir ~/.config/nvim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vimrc" > ~/.config/nvim/init.vim
+#echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after
+#let &packpath = &runtimepath
+#source ~/.vimrc" > ~/.config/nvim/init.vim
 vim +PluginInstall +qall

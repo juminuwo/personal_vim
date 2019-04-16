7babfe5d187960b43214654100f9266ab103e612
juminuwo <31014579+juminuwo@users.noreply.github.com>
2019-04-16 13:48:16 +0100

    added tmux config for escape delay

--- personal_vim/install.sh
+++ personal_vim/install.sh
@@ -5,10 +5,17 @@ if [ -f ~/.vimrc ]; then
 	mv ~/.vimrc ~/.vimrc_old
 fi
 cp $BASEDIR/.vimrc ~/.vimrc
-#apt install neovim
 mkdir ~/.config/nvim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+
+#apt install neovim
 #echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after
 #let &packpath = &runtimepath
 #source ~/.vimrc" > ~/.config/nvim/init.vim
-vim +PluginInstall +qall
+
+if [ -f ~/.tmux.conf ]; then
+	touch ~/.tmux.conf
+fi
+echo "set -sg escape-time 0" >> ~/.tmux.conf
